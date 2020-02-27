969186e9c94c285be2229d748491c2587780b006
David Vollbracht <david@flipstone.com>
2020-02-27 08:36:37 -0600

    Adds rep to demystify prelude functions
    I think I'd like to add more functions here in time, be there are all
    the ones we have right now from Eden's reps. I could imagine that maybe
    we might end up having DemystifyingPrelude be a re-occuring theme in the
    reps as we tackle more advanced topics as well.

---
+++ haskell/DemystifyingPrelude.hs
@@ -0,0 +1,27 @@
+{-
+
+- Write a function fmapList that does what fmap does for a List
+- Write a function fmapMaybe that does what fmap does for a Maybe
+- Write a function foldLList function that does what foldl does for a List
+
+-}
+module DemystifyingPrelude where
+
+fmapMaybe :: (a -> b) -> Maybe a -> Maybe b
+fmapMaybe functionA2B ma =
+  case ma of
+    Nothing -> Nothing
+    Just a -> Just $ functionA2B a
+
+fmapList :: (a -> b) -> [a] -> [b]
+fmapList functionA2B listA =
+  case listA of
+    [] -> []
+    (x:xs) -> functionA2B x : fmapList functionA2B xs
+
+foldLList :: (b -> a -> b) -> b -> [a] -> b
+foldLList functionBA2B b listA =
+  case listA of
+    [] -> b
+    (x:xs) ->
+      makeFoldL functionBA2B (functionBA2B b x) xs
