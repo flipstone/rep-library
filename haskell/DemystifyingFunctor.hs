8f997a123abe401e45251c960cdbea0c5bfe71be
David Vollbracht <david@flipstone.com>
2020-03-02 08:14:40 -0600

    Adds more reps to the DemystifyingFunctor set

--- haskell/DemystifyingFunctor.hs
+++ haskell/DemystifyingFunctor.hs
@@ -12,6 +12,20 @@ for diving deeper into functors.
 - Implement map for your list
 - Create Functor instance for your List
 
+- Write out a sample value using your types that is Maybe a List of Strings
+- Write out a sample value using your types that is a List of Maybe Strings
+
+- Write a function to get the length of all the strings in your Maybe List of Strings
+  - Once using the mapping fuctions you wrote for you types explicitly
+  - Once using the Functor instances via fmap
+
+- Write a function to get the length of all the strings in your List of Maybe Strings
+  - Once using the mapping fuctions you wrote for you types explicitly
+  - Once using the Functor instances via fmap
+
+- Finally, write a function that can get the length of all the strings in either
+  one of the values above by using the Functor instance of both your types.
+
 -}
 module DemystifyingFunctor where
 
@@ -45,3 +59,34 @@ mapMyList f myList =
     MyCons a restOfMyList ->
       MyCons (f a) (mapMyList f restOfMyList)
 
+instance Functor MyList where
+  fmap =
+    mapMyList
+
+maybeListOfStrings :: MyMaybe (MyList String)
+maybeListOfStrings =
+  MyJust (MyCons "Bob" (MyCons "April" MyEmptyList))
+
+listOfMaybeStrings :: MyList (MyMaybe String)
+listOfMaybeStrings =
+  MyCons (MyJust "April") (MyCons MyNothing (MyCons (MyJust "Bob") MyEmptyList))
+
+lengthOfStrings1 :: MyMaybe (MyList String) -> MyMaybe (MyList Int)
+lengthOfStrings1 strings =
+  mapMyMaybe (mapMyList length) strings
+
+lengthOfStrings2 :: MyMaybe (MyList String) -> MyMaybe (MyList Int)
+lengthOfStrings2 strings =
+  fmap (fmap length) strings
+
+lengthOfStrings3 :: MyList (MyMaybe String) -> MyList (MyMaybe Int)
+lengthOfStrings3 strings =
+  mapMyList (mapMyMaybe length) strings
+
+lengthOfStrings4 :: MyList (MyMaybe String) -> MyList (MyMaybe Int)
+lengthOfStrings4 strings =
+  fmap (fmap length) strings
+
+lengthOfStrings5 :: (Functor f, Functor g) => f (g String) -> f (g Int)
+lengthOfStrings5 strings =
+  fmap (fmap length) strings
