a21758faac29a3f248b6d60ff1b8de6dc809b68d
David Vollbracht <david@flipstone.com>
2020-03-08 09:02:39 -0500

    Adds Applicative Syntax reps to ApplyAndPure
    This makes the ApplyAndPure rep a big bigger than I would ideally like,
    but I think it's probably good to do in the same place that we are
    building `applyMaybe` and `applyList`. Maybe we'll come up with a way to
    split them out in future.

--- haskell/ApplyAndPure.hs
+++ haskell/ApplyAndPure.hs
@@ -1,5 +1,7 @@
 {-
 
+Apply and Pure:
+
 These reps lay the groundwork for starting to talk about Applicative by making
 sure the applicative functions for Maybe and List are deeply ingrained in your
 neural pathways.
@@ -16,6 +18,29 @@ and so forth
   * Note from the future: Notice that the signatures of applyList and zipList
     are the same. The pure function we wrote above has a special association
     with applyList, but not with zipList. More about this in the future.
+
+- Build a record to represent a pet with three fields: name, age, and species.
+
+- Create a Maybe value for each of the types of these three fields
+- Use your `applyMaybe` function to build a `Maybe Pet` from these three maybe value.
+  - Once using regular function application (with `applyMaybe` in front)
+  - A second time using `applyMaybe` as an infix function.
+
+- Evaluate your `Maybe Pet` value at the repl and make sure you understand
+  the result. What happens if any of the values is `Nothing`?
+
+- Create three lists of values -- one for each of the pet fields.
+
+- Use your `applyList` function (in infix notation) to build a list of pets
+  from tese values
+
+- Evaluate your list of pets at the repl and make sure you understand the the
+  result.
+
+- Build another Maybe Pet value from your 3 Maybe values using the Functor
+  and Applicative operators <$> and <*>
+
+- Build another [Pet] value from your 3 List values using <$> and <*>
 
 -}
 module ApplyAndPure where
@@ -59,3 +84,87 @@ zipList fs as =
 
     _ ->
       []
+
+data Pet =
+  Pet
+    { petName :: String
+    , petAge :: Int
+    , petSpecies :: Species
+    } deriving Show
+
+data Species
+  = Cat
+  | Dog
+  deriving Show
+
+maybeName :: Maybe String
+maybeName =
+  Just "Sparkles"
+
+maybeAge :: Maybe Int
+maybeAge =
+  Just 3
+
+maybeSpecies :: Maybe Species
+maybeSpecies =
+  Just Cat
+
+maybePet1 :: Maybe Pet
+maybePet1 =
+  applyMaybe (applyMaybe (fmap Pet maybeName) maybeAge) maybeSpecies
+
+{-
+   Produces:
+    Just (Pet {petName = "Sparkles", petAge = 3, petSpecies = Cat})
+-}
+
+maybePet2 :: Maybe Pet
+maybePet2 =
+  fmap Pet maybeName
+    `applyMaybe` maybeAge
+    `applyMaybe` maybeSpecies
+
+possibleNames :: [String]
+possibleNames =
+  ["Spot", "Dr. Doom"]
+
+possibleAges :: [Int]
+possibleAges =
+  [4,5]
+
+possibleSpecies :: [Species]
+possibleSpecies  =
+  [Dog, Cat]
+
+possiblePets1 :: [Pet]
+possiblePets1 =
+  fmap Pet possibleNames
+     `applyList` possibleAges
+     `applyList` possibleSpecies
+
+{-
+   Produces:
+      [ Pet {petName = "Spot", petAge = 4, petSpecies = Dog}
+      , Pet {petName = "Spot", petAge = 4 , petSpecies = Cat}
+      , Pet {petName = "Spot", petAge = 5, petSpecies = Dog}
+      , Pet {petName = "Spot", petAge = 5, petSpecies = Cat}
+      , Pet {petName = "Dr. Doom", petAge = 4, petSpecies = Dog}
+      , Pet {petName = "Dr. Doom", petAge = 4, petSpecies = Cat}
+      , Pet {petName = "Dr. Doom", petAge = 5, petSpecies = Dog}
+      , Pet {petName = "Dr. Doom", petAge = 5, petSpecies = Cat}
+      ]
+-}
+
+maybePet3 :: Maybe Pet
+maybePet3 =
+  Pet
+    <$> maybeName
+    <*> maybeAge
+    <*> maybeSpecies
+
+possiblePets2 :: [Pet]
+possiblePets2 =
+  Pet
+    <$> possibleNames
+    <*> possibleAges
+    <*> possibleSpecies
